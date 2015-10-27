b2aa8b6456d492d63d1ec4a396da4752fd119602
Dylan T. Moberg <dylanmoberg@msn.com>
2015-10-27 15:49:07 -0700

    adding css overlay on images

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
+  //scroll to about
   $("#about").on("click", function() {
     $('html, body').animate({
       scrollTop: $("#aboutSection").offset().top - 203
     }, 1000);
   });
+  //scroll to work, might need to make same function
   $("#work").on("click", function() {
     $('html, body').animate({
       scrollTop: $("#projectSection").offset().top 
     }, 1000);
   });
-  console.log('hello world');
+  //css overlay for projects
+  var overlay = '<div id="overlay"> </div>';
+  $(".projects").on("hover", function() {
+    $(this).append(overlay);
+  });
 });
